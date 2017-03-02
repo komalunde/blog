d951ed6f81e8c2ce671d7289c053f8ce73ab1d2b
komal <undekomal@gmail.com>
2017-03-02 18:58:29 +0530

    sign in page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
-from app import app
+from flask import render_template,flash,redirect
+from  app import app
+from .forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -13,4 +15,18 @@ def index():
                  'body':'The Avengers movie was so cool!'
              }
     ]
-    return render_template('index.html',title='Home',user=user,posts=posts)
+    """return render_template('index.html',
+                           title='Home',
+                           user=user,posts=posts)"""
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('login requested for OpenID="%s", remember_me=%s'%
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
